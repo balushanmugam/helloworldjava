0aa4d4860cdef4c0c8c31867e99332e6e633ada4
balushanmugam <baluredhat@gmail.com>
2022-07-19 12:11:50 +0530

    webhook2

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - github webhook1");
+    System.out.println("Hello World - github webhook2");
     }
 }
